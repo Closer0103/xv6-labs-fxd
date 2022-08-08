d6c7348cc549b1673a678b9f1fc9aca7e8179b26
Closer0103 <1252667827@qq.com>
2022-08-08 19:52:35 +0800

    lab3

--- kernel/exec.c
+++ kernel/exec.c
@@ -56,6 +56,13 @@ exec(char *path, char **argv)
       goto bad;
     if(loadseg(pagetable, ph.vaddr, ip, ph.off, ph.filesz) < 0)
       goto bad;
+    
+    /*
+    if(sz1 >= PLIC) 
+    {
+      goto bad;
+    }
+    */
   }
   iunlockput(ip);
   end_op();
@@ -107,6 +114,10 @@ exec(char *path, char **argv)
     if(*s == '/')
       last = s+1;
   safestrcpy(p->name, last, sizeof(p->name));
+
+  /*重新建立映射*/
+  uvmunmap(p->kernel_pgtbl, 0, PGROUNDUP(oldsz)/PGSIZE, 0);
+  kvmcopymappings(pagetable, p->kernel_pgtbl, 0, sz);
     
   // Commit to the user image.
   oldpagetable = p->pagetable;
@@ -116,6 +127,10 @@ exec(char *path, char **argv)
   p->trapframe->sp = sp; // initial stack pointer
   proc_freepagetable(oldpagetable, oldsz);
 
+  if(p->pid==1)
+  {
+    vmprint(p->pagetable);
+  }
   return argc; // this ends up in a0, the first argument to main(argc, argv)
 
  bad:
